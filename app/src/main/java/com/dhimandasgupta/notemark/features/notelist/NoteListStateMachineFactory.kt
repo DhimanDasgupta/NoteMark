54e12b160bb5ba1332ae02b118c75be407fdaae3
Dhiman Dasgupta <dhiman.dasgupta1984@gmail.com>
2026-07-19 10:13:12 +0530

    Fix: Ensure distinct notes in NoteList by filtering duplicates before sorting
    - Applied `distinctBy { note.id }` to remove duplicate notes based on their IDs before sorting by `lastEditedAt`.

--- app/src/main/java/com/dhimandasgupta/notemark/features/notelist/NoteListStateMachineFactory.kt
+++ app/src/main/java/com/dhimandasgupta/notemark/features/notelist/NoteListStateMachineFactory.kt
@@ -79,7 +79,7 @@ class NoteListStateMachineFactory(
               NoteListStateWithNotes(
                 userName = userName,
                 loading = false,
-                notes = notes.sortedByDescending { it.lastEditedAt },
+                notes = notes.distinctBy { note -> note.id }.sortedByDescending { it.lastEditedAt },
               )
             }
           } else {
